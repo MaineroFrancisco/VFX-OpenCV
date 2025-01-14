4d015e0aeb5dfb2103b4fff5474613f08397f682
Francisco Mainero <mainerofrancisco@gmail.com>
2025-01-14 11:01:26 -0300

    Added comment

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include <GL/glew.h>
+#include <GL/glew.h> // TODO: necessary? remove
 #include <GLFW/glfw3.h>
 
 int main() {
